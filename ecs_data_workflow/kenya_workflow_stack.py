2023e4915f7432cd30f3b03017f5202f93f393ea
aryton <atediarjo@gmail.com>
2022-12-24 13:15:39 -0800

    add changes

--- ecs_data_workflow/kenya_workflow_stack.py
+++ ecs_data_workflow/kenya_workflow_stack.py
@@ -25,7 +25,7 @@ from aws_cdk import (
 from constructs import Construct
 
 
-class EcsDataWorkflowStack(Stack):
+class KenyaWorkflowStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
