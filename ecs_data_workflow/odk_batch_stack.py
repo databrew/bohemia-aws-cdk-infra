c083de1ee0c9499f53f297a50df44c703e762bbf
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-08-09 13:03:37 -0700

    update odk batch code

--- ecs_data_workflow/odk_batch_stack.py
+++ ecs_data_workflow/odk_batch_stack.py
@@ -142,7 +142,7 @@ class OdkBatchStack(Stack):
 
         # consolidate into state machines
         state_machine = sfn.StateMachine(
-            self, "KenyaDataPipeline",
+            self, "ODKBatch",
             definition = parallel)
         
         #######################################
@@ -151,7 +151,7 @@ class OdkBatchStack(Stack):
 
         # add event rule to run data pipeline for work time at EAT
         update_schedule = events.Rule(
-            self, "KenyaDataPipelineTriggerWorkHoursSchedule",
+            self, "ODKBatchRefreshRate",
             schedule=events.Schedule.expression("rate(10 minutes)"),
             targets=[targets.SfnStateMachine(state_machine)]
         )
