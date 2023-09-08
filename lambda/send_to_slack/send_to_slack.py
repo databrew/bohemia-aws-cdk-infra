b4aa3160d8c7adf9c337643a2fd94aeae4b57f88
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-09-08 15:20:02 +0700

    add logging

--- lambda/send_to_slack/send_to_slack.py
+++ lambda/send_to_slack/send_to_slack.py
@@ -1,24 +1,28 @@
 import urllib3 
 import boto3
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 http = urllib3.PoolManager() 
 
 def get_slack_webhook_url(parameter_name):
     # Initialize a Boto3 SSM client
     ssm_client = boto3.client('ssm', region_name='us-east-1')
     try:
+        logger.info('Try fetching SLACK URL parameter from SSM')
         # Get the parameter value
         response = ssm_client.get_parameter(Name=parameter_name, WithDecryption=True)
         # Extract the value from the response
         parameter_value = response['Parameter']['Value']
     except ssm_client.exceptions.ParameterNotFound:
-        print(f"Parameter '{parameter_name}' not found in SSM.")
+        logger.error(f"Parameter '{parameter_name}' not found in SSM.")
     except Exception as e:
-        print(f"Error: {str(e)}")
-
+        logger.error(f"Error: {str(e)}")
     return(parameter_value)
 
 def lambda_handler(event, context): 
+    logger.info('Fetch event from SQS Queue')
     payload = event['Records'][0]['body']
     url = get_slack_webhook_url('SLACK_WEBHOOK_URL')
     resp = http.request('POST', url, body=payload)
