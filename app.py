ffdb31834bd4565f4f1b4d1849a57e9d67400ddb
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-09-25 17:54:15 +0700

    add odk backup stack

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from ecs_data_workflow.cloudfront_report_stack import CloudFrontReportStack
 from ecs_data_workflow.glue_infra_stack import GlueInfraStack
 from ecs_data_workflow.odk_batch_stack import OdkBatchStack
 from ecs_data_workflow.slack_notification_stack import SlackNotificationStack
-from ecs_data_workflow.metadata_data_quality_test_stack import MetadataDataQualityStack
+from ecs_data_workflow.odk_backup import OdkBackupStack
 
 # instantiate application
 app = cdk.App()
@@ -55,7 +55,6 @@ kenya_workflow = KenyaWorkflowStack(
     cluster = base_infra.cluster
 )
 
-
 # this is the glue database setup
 glue_db = GlueInfraStack(
     app, "GlueInfraStack",
@@ -72,8 +71,12 @@ slack_notification = SlackNotificationStack(
 kenya_workflow.add_dependency(odk_batch)
 slack_notification.add_dependency(kenya_workflow)
 
+# backup 
+odk_backup = OdkBackupStack(
+    app,
+    "OdkBackupStack"
+)
 
-metadata_testing = MetadataDataQualityStack(app, "MetadataDataqualityStack")
 
 # synthesize to cloudformation
 app.synth()
---
+++ lambda/odk_backup/lambda_function.py
@@ -0,0 +1,91 @@
+# This script is used for capturing ODK Backup via API
+# Author: atediarjo@gmail.com
+
+import os
+import json
+import requests
+import boto3
+import logging
+import time
+from datetime import datetime
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3 = boto3.client('s3')
+secrets_manager = boto3.client('secretsmanager')
+
+CURRENT_DATE = datetime.utcnow().strftime('%Y-%m-%d')
+OUTPUT_FILEPATH = f'/tmp/databrew-odk-central-backup.zip'
+OBJECT_NAME = os.path.basename(OUTPUT_FILEPATH)
+SECRET_ID = os.getenv('SECRET_ID')
+ENDPOINT = 'https://databrew.org/v1/backup'
+AUTH_DICT = json.loads(
+    secrets_manager.get_secret_value(
+        SecretId = SECRET_ID)['SecretString']
+)
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+# Handler, event will go through here
+def lambda_handler(event, context):
+    start = time.time()
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    # try POST request to ODK
+    try:
+        response = requests.post(
+            ENDPOINT, 
+            headers = headers,
+            auth= (AUTH_DICT['username'], AUTH_DICT['password'])
+        )
+    except Exception as e:
+        logging.error(str(e))
+
+    # # Check if the request was successful
+    if response.status_code == 200:
+        # Assuming the response contains the ZIP file as content
+        zip_file_content = response.content
+        
+        # Save the ZIP backup to /tmp
+        with open(OUTPUT_FILEPATH, "wb") as f:
+            f.write(zip_file_content)
+        logging.info("ZIP file downloaded successfully.")
+    else:
+        logging.info("Error:", response.status_code)
+
+    # upload s3 file to bucket
+    upload_file(
+        file_name = OUTPUT_FILEPATH,
+        bucket = os.getenv('BUCKET_NAME'),
+        object_name = OBJECT_NAME
+    )
+
+    end = time.time()
+    logging.info(f'Lambda ran for {int(end - start)} secs')
