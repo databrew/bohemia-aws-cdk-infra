cf1e284f67740db4d901f27ea192364c47e9fb34
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2023-11-13 14:33:34 +0700

    updated lambda with unload

--- lambda/athena/lambda_function.py
+++ lambda/athena/lambda_function.py
@@ -5,28 +5,33 @@ import random
 import logging
 import time
 
-BUCKET_TARGET = os.getenv('OUTPUT_BUCKET_NAME')
-FOLDER_NAME = 'test'
+BUCKET_TARGET=os.getenv('OUTPUT_LAKE_DB_BUCKET_NAME')
+FOLDER_NAME='test'
 SQL_STATEMENT = """
 SELECT extid FROM metadata.safety
 """
 
-# Set the query string. This should be a valid Athena SQL query.
-run_query = f"CREATE TABLE test WITH (format = 'PARQUET', external_location='{BUCKET_TARGET}/{FOLDER_NAME}/', write_compression='SNAPPY') AS {SQL_STATEMENT}" 
-
 def lambda_handler(event, context):
     s3_client = boto3.client('s3')
     athena_client = boto3.client('athena')
-    
+
+    # Set the query string. This should be a valid Athena SQL query.
+    run_query = f"UNLOAD ({SQL_STATEMENT}) TO 's3://{BUCKET_TARGET}/bohemia_prod/dwh/{FOLDER_NAME}/' WITH (format = 'PARQUET', compression='SNAPPY')" 
+
+    # cleanpath
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_TARGET)
+    bucket.objects.filter(Prefix=f"bohemia_prod/dwh/{FOLDER_NAME}/").delete()
+
     # Execute the query
     response = athena_client.start_query_execution(
-        QueryString=run_query,
-        WorkGroup="primary",
+            QueryString=run_query,
+            WorkGroup="primary",
     )
 
     # Get the execution ID of the query from the response
     query_execution_id = response["QueryExecutionId"]
-    
+
     # Poll the query execution until it finishes
     query_status = "RUNNING"
     while query_status == "RUNNING" or query_status == "QUEUED":
@@ -35,11 +40,12 @@ def lambda_handler(event, context):
         )
         query_status = response["QueryExecution"]["Status"]["State"]
         if query_status == "FAILED":
-            raise Exception("Query failed to execute")
+            err_message = response["QueryExecution"]['Status']['StateChangeReason']
+            raise Exception(err_message)
         elif query_status == "CANCELLED":
             raise Exception("Query was cancelled")
         time.sleep(1)
-    
+
     # Get the query results
     response = athena_client.get_query_results(QueryExecutionId=query_execution_id)
         
@@ -47,5 +53,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response)
     }
