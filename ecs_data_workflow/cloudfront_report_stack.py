2ad1582335f930e880277add40a874febec48374
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2024-01-13 14:07:10 +0700

    added bucket for monitoring icf

--- ecs_data_workflow/cloudfront_report_stack.py
+++ ecs_data_workflow/cloudfront_report_stack.py
@@ -14,7 +14,9 @@ class CloudFrontReportStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        output_bucket_name = os.getenv('BUCKET_PREFIX') + "bohemia-reporting"
+
+        # data-ops reporting bucket
+        bucket_name = os.getenv('BUCKET_PREFIX') + "bohemia-reporting"
         bucket = s3.Bucket(
             self, "CFBucket",
             bucket_name= output_bucket_name,
@@ -33,3 +35,27 @@ class CloudFrontReportStack(Stack):
         cdk.CfnOutput(self, "DistributionID", 
                       value=distribution.distribution_id,
                       export_name='cf-distribution-id')
+
+
+        # Monitoring ICF reporting bucket
+        output_bucket_name = os.getenv('BUCKET_PREFIX') + "bohemia-reporting-icf-monitoring"
+        monitoring_icf_bucket = s3.Bucket(
+            self, "MonitoringICFBucket",
+            bucket_name= output_bucket_name,
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED
+        )
+
+        monitoring_icf_distribution = cloudfront.Distribution(
+            self, "MonitoringICFDistribution",
+            default_root_object= 'index.html',
+            default_behavior=cloudfront.BehaviorOptions(origin=origins.S3Origin(monitoring_icf_bucket))
+        )
+
+        cdk.CfnOutput(self, "MonitoringICFBucketArn", value=monitoring_icf_bucket.bucket_arn)
+        cdk.CfnOutput(self, "MonitoringICFDistributionURL", value=monitoring_icf_distribution.distribution_domain_name)
+        cdk.CfnOutput(self, "MonitoringICFDistributionID", 
+                      value=monitoring_icf_distribution.distribution_id,
+                      export_name='monitoring-icf-cf-distribution-id')
+
+
