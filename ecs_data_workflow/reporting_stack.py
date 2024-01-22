6396619b02916f122991bb6e9e18609f0a5e78cf
Aryton Tediarjo <35942857+arytontediarjo@users.noreply.github.com>
2024-01-22 21:20:29 +0100

    update mem

--- ecs_data_workflow/reporting_stack.py
+++ ecs_data_workflow/reporting_stack.py
@@ -116,7 +116,7 @@ class ReportingStack(Stack):
             execution_role=ecs_role,
             task_role=ecs_role,
             family='pipeline-reporting',
-            memory_limit_mib= 16000,
+            memory_limit_mib= 16384,
             cpu=2048
         )
 
@@ -128,7 +128,7 @@ class ReportingStack(Stack):
             "pipeline-reporting-container",
             image=ecs.ContainerImage.from_registry(dockerhub_image),
             logging=ecs.LogDriver.aws_logs(stream_prefix="kenya-logs"),
-            memory_limit_mib=16000,
+            memory_limit_mib=16384,
             cpu=2048
         )
 
